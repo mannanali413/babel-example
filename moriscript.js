1b4ab3e64cd7f3b71510d23af8a4cd9478b13fce
Mir Ali <mannanali413@gmail.com>
2017-05-16 17:57:48 +0530

    a simple babel plugin example

---
+++ moriscript.js
@@ -0,0 +1,72 @@
+module.exports = function(babel){
+	var t = babel.types;
+
+	function moriMethod(name){
+		var expr = t.memberExpression(
+			t.identifier('mori'),
+			t.identifier(name)
+			);
+		expr.isClean = true;
+		return expr;
+	}
+
+	return {
+		visitor: {
+			ArrayExpression: function(path){
+				path.replaceWith(
+					t.callExpression(
+						moriMethod('vector'),
+						path.node.elements
+						)
+					);
+			},
+			ObjectExpression: function(path){
+				var props = [];
+				path.node.properties.forEach(function(prop){
+					props.push(
+						t.stringLiteral(prop.key.name),
+						prop.value
+						);
+				});
+				path.replaceWith(
+					t.callExpression(
+						moriMethod('hashMap'),
+						props
+						)
+					);
+			},
+			AssignmentExpression: function(path){
+				var lhs = path.node.left,
+					rhs = path.node.right;
+
+				if(t.isMemberExpression(lhs)){
+					if(t.isIdentifier(lhs.property)){
+						lhs.property = t.stringLiteral(lhs.property.name)
+					}
+
+					path.replaceWith(
+						t.callExpression(
+							moriMethod('assoc'),
+							[lhs.object, lhs.property, rhs]
+							)
+						)
+				}
+			},
+			MemberExpression: function(path){
+				if(path.node.isClean) return;
+				if(t.isAssignmentExpression(path.parent)) return;
+
+				if(t.isIdentifier(path.node.property)){
+					path.node.property = t.stringLiteral(path.node.property.name);
+				}
+
+				path.replaceWith(
+					t.callExpression(
+						moriMethod('get'),
+						[path.node.object, path.node.property]
+						)
+					)
+			}
+		}
+	}
+}
